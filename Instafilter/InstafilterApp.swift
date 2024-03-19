09bed01d79147dd48a84883c32504b186bf300bf
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-03-19 20:40:38 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by charlene hoareau on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
